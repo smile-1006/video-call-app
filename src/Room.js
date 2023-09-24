161c742c8dbf2fbff96f127f1ed64087f3d1dfe1
Smile <singhswapnil060303@gmail.com>
2023-09-24 22:10:53 +0530

    mymeeting details

--- src/Room.js
+++ src/Room.js
@@ -1,6 +1,15 @@
 import React from 'react'
+//it will provide some inbuild functions
+import {ZegoUIKitPrebuild } from "@zegocloud/zego-uikit-prebuilt";
+
 
 function Room() {
+    let myMeeting = async (Element)=>{
+        const appID = ;
+        const serverSecret= "";
+        
+
+    }
     return (
         <div>
             swapnil
